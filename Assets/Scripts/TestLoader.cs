0760200d0e191761be20f66bbb74142434516e16
Li TsungYi <tsungyi.li@aktsk.com>
2019-04-10 18:39:12 +0800

    Add loader

---
+++ Assets/Scripts/TestLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Akatsuki
+{
+    public class TestLoader : MonoBehaviour
+    {
+        [SerializeField]
+        private PrefabComponent m_goodPrefab;
+
+        [SerializeField]
+        private PrefabComponent m_missingPrefab;
+
+        private void Awake()
+        {
+            Instantiate(m_goodPrefab, transform);
+            Instantiate(m_missingPrefab, transform);
+        }
+    }
+}
